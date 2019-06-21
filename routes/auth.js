a74a4f59fa431e78e1a368a2236a6fed1ea857a6
rkritchat <rkritchat@gmail.com>
2019-06-22 00:28:48 +0700

    add logic on login function

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const express = require('express')
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
 const router = express.Router()
+const User = require('../model/User')
 
 // @route    GET api/auth
 // @desc     Get logged in user
@@ -12,8 +16,52 @@ router.get('/', (req, res) => {
 // @route    Post api/auth
 // @desc     Auth user & get token
 // @access   Public
-router.post('/', (req, res) => {
-  res.send('Log in user')
-})
+router.post(
+  '/',
+  [
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const erros = validationResult(req)
+    if (!erros.isEmpty()) {
+      return res.status(400).json({ errors: erros.array() })
+    }
+
+    const { email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' })
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send('Server error')
+    }
+  }
+)
 
 module.exports = router
