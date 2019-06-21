ff2dfab9cea7e7b994718737e82230d320e0f466
rkritchat <rkritchat@gmail.com>
2019-06-21 23:43:58 +0700

    Create db connection

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const connectDb = require('./config/db')
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+connectDb()
 
 app.get('/', (req, res) => {
   res.json({ msg: 'welcome to the ContactKeeper API' })
