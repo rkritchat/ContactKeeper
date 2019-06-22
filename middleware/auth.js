8b306e38b8405f770b9a5a380bb6d72940323f8d
rkritchat <rkritchat@gmail.com>
2019-06-22 10:26:32 +0700

    Add logic on contacts

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const config = require('config')
 module.exports = (req, res, next) => {
     const token = req.header('x-auth-token')
 
-    if (!token) return res.status(401).json({ msg: 'No token, autjorization denied' })
+    if (!token) return res.status(401).json({ msg: 'No token, authorization denied' })
 
     try {
         const decode = jwt.verify(token, config.get('jwtSecret'))
